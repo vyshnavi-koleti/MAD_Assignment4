643d0e2b83fa98be0b949b5e533d4c1a13f7ae91
vyshnavi-koleti <89739578+vyshnavi-koleti@users.noreply.github.com>
2023-09-21 15:08:48 -0600

    Add files via upload

---
+++ Assignment4/Assignment4/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Assignment4
+//
+//  Created by Vyshnavi Koleti on 9/21/23.
+//
+
+import SwiftUI
+
+struct Country: Codable, Identifiable {
+    var id: Int { return UUID().hashValue }
+    var name: CountryName
+    var capital: [String]?
+    var flag: String
+    var population: Int
+}
+
+struct CountryName: Codable {
+    var common: String
+    var official: String
+}
+
+struct ContentView: View {
+    
+    @State var countries =  [Country]()
+    @State private var showingAlert = false
+    
+    func getAllCountries() async -> () {
+        do {
+            let url = URL(string: "https://restcountries.com/v3.1/all")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print(data)
+            countries = try JSONDecoder().decode([Country].self, from: data)
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List(countries) { country in
+                VStack(alignment: .leading) {
+                    
+                    
+//                    Text("\(country.flag) • \(country.name.common)")
+                    
+                    Button("\(country.flag) • \(country.name.common)"){
+                        showingAlert = true
+                    }
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("\(country.name.common)"), message: Text("\(country.population)"), dismissButton: .default(Text("Got it!")))
+                    }
+                    
+                }
+            }
+            .task {
+                await getAllCountries()
+            }
+        }
+        .navigationTitle("Countries")
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
